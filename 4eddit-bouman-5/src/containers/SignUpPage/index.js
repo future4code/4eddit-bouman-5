4928547eac5ae5d2f3bc5f91633c206652afd9a9
Yan Sabino <yan.sabino@hotmail.com>
2020-01-20 14:40:11 -0300

    signup form feito

--- 4eddit-bouman-5/src/containers/SignUpPage/index.js
+++ 4eddit-bouman-5/src/containers/SignUpPage/index.js
@@ -6,15 +6,59 @@ import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { routes } from '../Router/index'
 
+const signUpForm = [
+    {
+       name: 'name',
+       type: 'text',
+       label: 'Usuário ',
+       required: true,
+       pattern: "[A-Za-z]"
+    },
+    {
+       name: 'email',
+       type: 'email',
+       label: 'E-mail: ',
+       required: true,
+       pattern: "[A-Za-^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$]{3,}"
+    },
+    {
+        name: 'password',
+        type: 'password',
+        label: 'password',
+        required: true,
+    },
 
+]
+
+const SignUpWrapper = styled.form`
+  width: 100%;
+  height: 100vh;
+  gap: 10px;
+  place-content: center;
+  justify-items: center;
+  display: grid;
+`;
 
 
 class SignUpPage extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-
+            form: {}
         }
+    }
+
+    handleFieldChange = event => {
+        const { name, value } = event.target
+        console.log(name,value)
+        this.setState({
+          form: { ...this.state.form, [name]: value }
+        });
+    };
+
+    handleOnSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.form)
     }
 
     render(){
@@ -23,7 +67,21 @@ class SignUpPage extends React.Component{
 
         return(
             <div>
-                <Button onClick={goToLoginPage}> Sign Up </Button>
+                <SignUpWrapper onSubmit={this.handleOnSubmit}>
+                    <h1>Cadastre-se</h1>
+                    {signUpForm.map(input =>(
+                        <TextField
+                            onChange={this.handleFieldChange}
+                            name={input.name}
+                            type={input.type}
+                            label={input.label}
+                            required={input.required}
+                            pattern={input.pattern}
+                        />
+                    ))}
+                    <Button onClick={goToLoginPage}> Sign Up </Button>
+                </SignUpWrapper>
+                
             </div>
         )
     }
